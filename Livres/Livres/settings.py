ecb702967a9e463fc0d91b99d07c47732ebb1eb1
closzach <constantinloszach@gmail.com>
2024-09-24 08:08:47 +0200

    Modification des paramètres

--- Livres/Livres/settings.py
+++ Livres/Livres/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'LivresListe',
 ]
 
 MIDDLEWARE = [
@@ -103,13 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
 USE_TZ = True
+
+DATE_INPUT_FORMATS = ('%d/%m/%Y','%Y-%m-%d')
 
 
 # Static files (CSS, JavaScript, Images)
