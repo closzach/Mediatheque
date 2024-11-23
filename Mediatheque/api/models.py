77bdd378642980624b48af6e2c3bc8dbadd115c3
closzach <constantinloszach@gmail.com>
2024-11-23 14:50:40 +0100

    Renommage des images lors de l'enregistrement

--- Mediatheque/api/models.py
+++ Mediatheque/api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import os
+from django.utils.timezone import now
+from PIL import Image
 
 class Serie(models.Model):
     nom = models.CharField(max_length=100)
@@ -29,17 +32,30 @@ class Lecteur(models.Model):
 
     def __str__(self):
         return self.nom
-
+    
+def renommer_image(instance, filename):
+        extension = os.path.splitext(filename)[1]
+        nouveau_nom = f"{instance.nom}_{now().strftime('%Y%m%d%H%M%S')}{extension}"
+        return os.path.join("livres", nouveau_nom)
 class Livre(models.Model):
     nom = models.CharField(max_length=100)
     date_sortie = models.DateField()
     nombre_pages = models.IntegerField()
     isbn = models.CharField(max_length=100, unique=True, blank=True, null=True)
-    image = models.ImageField(upload_to="livres", default="default.png", blank=True)
+    image = models.ImageField(upload_to=renommer_image, default="default.png", blank=True)
 
     serie = models.ForeignKey(Serie, on_delete=models.CASCADE, null=True, blank=True)
     auteurs = models.ManyToManyField(Auteur)
     tags = models.ManyToManyField(Tag)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            img_path = self.image.path
+            with Image.open(img_path) as img:
+                max_size = (800, 1200)
+                img.thumbnail(max_size)
+                img.save(img_path)
 
     def __str__(self):
         return self.nom
